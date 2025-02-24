811a49282f704fbbe725c6aa701907f6f511141b
Dave <contatodrss@gmail.com>
2025-02-24 03:22:32 -0400

    Initial commit

---
+++ src/main/java/com/daveyo/sboot_java_02_avnd_JPA/SbootJava02AvndJpaApplication.java
@@ -0,0 +1,13 @@
+package com.daveyo.sboot_java_02_avnd_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootJava02AvndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootJava02AvndJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daveyo/sboot_java_02_avnd_JPA/SbootJava02AvndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daveyo.sboot_java_02_avnd_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootJava02AvndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
